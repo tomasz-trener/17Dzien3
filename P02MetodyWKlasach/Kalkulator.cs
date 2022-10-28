59f9f5fd8ae1faecb22cd614148c7c84122703a8
kurs <kurs@DESKTOP-V3HDKPQ>
2022-10-28 08:59:17 +0200

    Add project files.

---
+++ P02MetodyWKlasach/Kalkulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02MetodyWKlasach
+{
+    internal class Kalkulator
+    {
+
+        public int Dodaj(int a, int b)
+        {
+            int liczba1;
+            int liczba2;
+
+            liczba1 = a;
+            liczba2 = b;
+
+            int wynik = liczba1 + liczba2;
+
+            return wynik;
+        }
+
+
+    }
+}
---
+++ P02MetodyWKlasach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02MetodyWKlasach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator k = new Kalkulator();
+
+            int a=  k.Dodaj(4, 5);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
